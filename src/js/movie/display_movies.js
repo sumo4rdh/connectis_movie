bfabe9e4d6a8949bd2b671d26c561ec1c3da117f
Artur Brzozowski <artur.brzozowski@raffine.eu>
2020-06-21 10:25:03 +0200

    added display of movies titles on page

--- src/js/movie/display_movies.js
+++ src/js/movie/display_movies.js
@@ -1,5 +1,10 @@
 export default function(movieArray) {
+  const mainDiv = document.getElementById("main-content");
   movieArray.forEach((item, i) => {
     console.log(item);
+    let { title } = item;
+    var div = document.createElement("div");
+    div.innerHTML = title;
+    mainDiv.appendChild(div);
   });
 }
--- src/js/movie/fetch.js
+++ src/js/movie/fetch.js
@@ -3,8 +3,6 @@ import displayMovies from "./display_movies";
 import { API_URL, MOVIE_LIST } from "../config/api";
 
 export default function() {
-  console.log(API_URL);
-  let movies;
   axios.get(API_URL).then(response => {
     MOVIE_LIST.push(...response.data.results);
     displayMovies(MOVIE_LIST);
